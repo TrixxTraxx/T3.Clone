caa23dc06927b60b2e0f535f5756afa1d6b22f81
trixxtraxx <kosimo.weiss@gmail.com>
2025-06-09 20:05:21 +0200

    ADD: Attachment Controller

---
+++ T3.Clone.Dtos/Messages/AttachmentDto.cs
@@ -0,0 +1,13 @@
+namespace T3.Clone.Dtos.Messages;
+
+public class AttachmentDto
+{
+    public int Id { get; set; }
+    
+    public string FileName { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public int? MessageId { get; set; }
+}
---
+++ T3.Clone.Server/Mappers/AttachmentMapper.cs
@@ -0,0 +1,12 @@
+using Riok.Mapperly.Abstractions;
+using T3.Clone.Dtos.Messages;
+using T3.Clone.Dtos.Model;
+using T3.Clone.Server.Data;
+
+namespace T3.Clone.Server.Mappers;
+
+[Mapper]
+public partial class AttachmentMapper
+{
+    public static partial AttachmentDto Map(MessageAttachment attachment);
+}
