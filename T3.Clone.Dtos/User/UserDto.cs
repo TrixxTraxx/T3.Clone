b76ef0cc76dab7c94354f4a6a7ec20ddfb996782
trixxtraxx <kosimo.weiss@gmail.com>
2025-06-10 22:11:25 +0200

    clear cache uppon Login and add switch account button

--- T3.Clone.Dtos/User/UserDto.cs
+++ T3.Clone.Dtos/User/UserDto.cs
@@ -5,4 +5,5 @@ public class UserDto
     public string UserName { get; set; }
     public string Email { get; set; }
     public string ProfilePictureUrl { get; set; }
+    public string DisplayName { get; set; }
 }
