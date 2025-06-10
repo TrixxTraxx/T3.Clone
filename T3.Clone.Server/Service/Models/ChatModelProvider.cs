8a08053b46f4a6d494872c3f0703eeb8d387df83
trixxtraxx <kosimo.weiss@gmail.com>
2025-06-10 16:54:00 +0200

    ADD: better Model Select Modal (will likely change that later)

--- T3.Clone.Server/Service/Models/ChatModelProvider.cs
+++ T3.Clone.Server/Service/Models/ChatModelProvider.cs
@@ -3,7 +3,10 @@ using T3.Clone.Server.Service.Models;
 
 namespace T3.Clone.Client.Services;
 
-public class ChatModelProvider(IServiceProvider serviceProvider)
+public class ChatModelProvider(
+    IServiceProvider serviceProvider,
+    ApplicationDbContext context
+)
 {
     private Dictionary<string, Type> _chatModels = new Dictionary<string, Type>()
     {
@@ -13,6 +16,13 @@ public class ChatModelProvider(IServiceProvider serviceProvider)
     public IChatModel GetChatModel(AiModel aiModel)
     {
         var type = _chatModels[aiModel.Provider];
+        return (serviceProvider.GetService(type) as IChatModel)!;
+    }
+    
+    public IChatModel GetThreadGenerationModel()
+    {
+        var defaultModel = context.AiModels.FirstOrDefault(x => x.IsDefault);
+        var type = _chatModels[defaultModel.Provider];
         return (serviceProvider.GetService(type) as IChatModel)!;
     }
 }
