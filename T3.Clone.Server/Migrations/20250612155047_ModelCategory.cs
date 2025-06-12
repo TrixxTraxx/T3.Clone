f49545f7f5b615346b7c60a3127bb033a14f5c89
trixxtraxx <kosimo.weiss@gmail.com>
2025-06-12 17:51:07 +0200

    ADD: Model Category

---
+++ T3.Clone.Server/Migrations/20250612155047_ModelCategory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace T3.Clone.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "AiModels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "AiModels");
+        }
+    }
+}
